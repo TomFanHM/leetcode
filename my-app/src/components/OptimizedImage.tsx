9effe03cf8d744d98eb89da24f91662d51ee0fe3
Tom Fan <119341778+TomFanHM@users.noreply.github.com>
2023-05-24 14:31:00 +0000

    update

---
+++ my-app/src/components/OptimizedImage.tsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+type OptimizedImageProps = {
+  src: string;
+  alt: string;
+  className: string;
+};
+
+export default function OptimizedImage(props: OptimizedImageProps) {
+  const [imageLoaded, setImageLoaded] = useState<boolean>(false);
+  const [imageError, setImageError] = useState<boolean>(false);
+  const handleImageLoad = () => {
+    setImageLoaded(true);
+  };
+
+  const handleImageError = () => {
+    setImageError(true);
+  };
+  return (
+    <img
+      src={imageError ? "/fallback.png" : props.src}
+      className={`${props.className} ${imageLoaded ? "filter grayscale" : ""}`}
+      loading="lazy"
+      onLoad={handleImageLoad}
+      onError={handleImageError}
+      alt={props.alt}
+    />
+  );
+}
